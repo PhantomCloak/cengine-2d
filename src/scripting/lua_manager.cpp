dbb4ac821ed4d00d0dbf63ea91a5ea9176a3a46f
PhantomCloak <phantom_clk@protonmail.com>
2022-12-22 00:49:53 +0300

    wip: lua loader

--- src/scripting/lua_manager.cpp
+++ src/scripting/lua_manager.cpp
@@ -1,4 +1,5 @@
 #include "lua_manager.h"
+#include "../log/log.h"
 #include <filesystem>
 
 sol::state LuaManager::luaState;
@@ -9,16 +10,48 @@ sol::table LuaManager::LoadLuaTable(std::string key) {
 
 void LuaManager::InitSandbox() {
     luaState.open_libraries(sol::lib::base);
+    luaState.open_libraries(sol::lib::package);
+    luaState.open_libraries(sol::lib::string);
     luaState.open_libraries(sol::lib::table);
+    luaState.open_libraries(sol::lib::math);
+    luaState.open_libraries(sol::lib::coroutine);
+
+    const std::string package_path = luaState["package"]["path"];
+
+    char* luaLibPathEnv = std::getenv("COMMANCHE_LUA_LIBS");
+    std::string luaLibPath;
+
+    if (luaLibPathEnv != nullptr) {
+        luaLibPath = std::string(luaLibPathEnv);
+    } else {
+        luaLibPath = "./assets/scripts/lib";
+    }
+
+    Log::Inf("lua path pointing to " + luaLibPath);
+
+    luaState["package"]["path"] = package_path + ";" + luaLibPath;
 }
 
 void LuaManager::LoadLuaFilesInDirectory(std::string path) {
     for (const auto& entry : std::filesystem::directory_iterator(path)) {
         luaState.script_file(entry.path());
+        continue;
+        sol::load_result luaScript = luaState.load_file(entry.path());
+
+        if (!luaScript.valid()) {
+            sol::error err = luaScript;
+            std::string msg = err.what();
+            Log::Err("error loading lua file: " + msg);
+            continue;
+        }
+        Log::Inf("Lua script at " + entry.path().string() + " has loaded successfully.");
+        sol::protected_function_result result = luaScript();
+        if (!result.valid()) {
+            continue;
+        }
     }
 }
 
-//template <typename... Args>
-//void LuaManager::RegisterCppToLuaFunc(const std::string key, Args&&... args) {
-    //luaState.set_function(key.c_str(), std::forward<Args>(args)...);
-//}
+void LuaManager::LoadLuaFile(const std::string path) {
+    luaState.script_file(path);
+}
--- src/scripting/lua_manager.h
+++ src/scripting/lua_manager.h
@@ -7,6 +7,7 @@ class LuaManager {
     static sol::state luaState;
     static void InitSandbox();
     static void LoadLuaFilesInDirectory(const std::string path);
+    static void LoadLuaFile(const std::string path);
     static sol::table LoadLuaTable(std::string key);
     template <typename... Args>
     static void RegisterCppToLuaFunc(const std::string key, Args&&... args){
