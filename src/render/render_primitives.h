545e48b8e0cd215fb5d42f112f0c7817a7831596
Phantom <ph4nt0m@192.168.1.107>
2023-10-15 20:52:21 +0300

    add: conditional include for win

--- src/render/render_primitives.h
+++ src/render/render_primitives.h
@@ -62,9 +62,9 @@ struct glRectTangle {
 	#define NOUSER            // All USER defines and routines
 #endif
 
-#include <Windows.h> // or any library that uses Windows.h
 
 #if defined(_WIN32)           // raylib uses these names as function parameters
+  #include <Windows.h> // or any library that uses Windows.h
 	#undef near
 	#undef far
 #endif
