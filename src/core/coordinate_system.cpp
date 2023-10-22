abc41866117b87af1a54ed8f2abd43c778b539a7
Phantom <ph4nt0m@Phantoms-MacBook-Air.local>
2023-10-22 11:49:58 +0300

    fix: coord issue

--- src/core/coordinate_system.cpp
+++ src/core/coordinate_system.cpp
@@ -9,8 +9,6 @@ int CoordinateCalculator::referenceHeight = 1080;
 float CoordinateCalculator::meterPixelRatioX = 100;   // X: 1 meter = 100 pixels at 1920x1080
 float CoordinateCalculator::meterPixelRatioY = 56.25; // Y: 1 meter = 56.25 pixels at 1920x1080
 
-extern "C" double getScreenScaleFactor();
-
 void CoordinateCalculator::ConvertMetersToPixels(float& xMeter, float& yMeter) {
     float scale = 1;
 
@@ -23,11 +21,12 @@ void CoordinateCalculator::ConvertMetersToPixels(float& xMeter, float& yMeter) {
 
 void CoordinateCalculator::ConvertPixelsToMeters(float& xPixel, float& yPixel) {
     float scale = 1;
+    
     float scaleX = (Editor::Instance->GetViewPortSize().x * scale) / static_cast<float>(referenceWidth);
     float scaleY = (Editor::Instance->GetViewPortSize().y * scale) / static_cast<float>(referenceHeight);
 
-    xPixel = xPixel / (referenceWidth / meterPixelRatioX) * scaleX;
-    yPixel = yPixel / (referenceHeight / meterPixelRatioY) * scaleY;
+    xPixel = (xPixel / scaleX) / (referenceWidth / meterPixelRatioX);
+    yPixel = (yPixel / scaleY) / (referenceHeight / meterPixelRatioY);
 }
 
 void CoordinateCalculator::NormalizePixelCoord(float& xPixel, float& yPixel) {
